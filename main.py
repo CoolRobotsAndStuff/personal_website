a11c384a7dfb3face2286fadaadbfbc502a0a6bd
ale <aledeum.saf@gmail.com>
2023-11-08 18:34:36 -0300

    added favicon and light and dark themes

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
-from flask import Flask, render_template, render_template_string, request, redirect, url_for, abort
+from flask import Flask, render_template, render_template_string, request, redirect, url_for, abort, send_from_directory
 import werkzeug
+import os
 
 
 
 app = Flask(__name__)
 
+
 supported_languages = ["en", "es"]
 default_lang = "en"
+
+supported_themes = ("light", "dark")
+default_theme = "dark"
 
 def get_lang():
     return werkzeug.datastructures.LanguageAccept([(al[0][0:2], al[1]) for al in request.accept_languages]).best_match(supported_languages)
 
+def get_theme():
+    theme = request.args.get("theme", default=default_theme, type=str)
+    print("theme:", theme)
+    return theme
+
 def join_base_with_content(lang, content_html_name):
-    content = render_template(f"{lang}/{content_html_name}", lang=lang)
+    content = render_template(f"{lang}/{content_html_name}", lang=lang, theme=get_theme())
     
     template = f'''{{% extends "{lang}/base.html" %}}\n 
                    {{% block content %}} {content} {{% endblock %}}
                    '''
-    return render_template_string(template, lang=lang)
+    return render_template_string(template, lang=lang, theme=get_theme())
 
-def go_to_default(page):
+def go_to_default(page, theme=default_theme):
     lang = get_lang()
-    return redirect(url_for(page, lang=lang))
+    return redirect(url_for(page, lang=lang, theme=theme))
+
 
 @app.route("/")
 def root():
@@ -29,11 +40,13 @@ def root():
 
 @app.route("/<lang_or_page>/")
 def root_lang(lang_or_page):
+    
+
     if lang_or_page in supported_languages:
-        return redirect(url_for("home", lang=lang_or_page))
+        return redirect(url_for("home", lang=lang_or_page, theme=get_theme()))
     else:
         try:
-            return go_to_default(lang_or_page)
+            return go_to_default(lang_or_page, theme=get_theme())
         except werkzeug.routing.exceptions.BuildError:
             abort(404)
 
