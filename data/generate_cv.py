f491f2fe695c815dad8e6e3d6c0d2b3550729c8e
ale <aledeum.saf@gmail.com>
2023-11-26 21:49:49 -0300

    made pdf converter

--- data/generate_cv.py
+++ data/generate_cv.py
@@ -1,11 +1,32 @@
 import yaml
+import pdfkit
+from flask import render_template, render_template_string, Flask
 
-from flask import render_template
 
-def get_cv():
-    with open("./cv_data/cv_data.yaml", "r") as file:
+def get_cv_string(lang):
+    with open(f"./data/{lang}/cv_data.yaml", "r") as file:
         data = yaml.safe_load(file)
 
-    return render_template("cv.html", data=data)
+    with open(f"./templates/cv.html", "r") as file:
+        temp = file.read()
 
+    return render_template_string(temp, data=data, theme="light")
+
+def cv_to_pdf(lang):
+    options = {
+    'page-size': 'A4',
+    'margin-top': '0.75in',
+    'margin-right': '0.75in',
+    'margin-bottom': '0.75in',
+    'margin-left': '0.75in',
+    'encoding': "UTF-8",
+    }
+    content = get_cv_string(lang)
+    pdfkit.from_string(content, "cv.pdf", options=options)
+
+
+if __name__ == "__main__":
+    app = Flask(__name__)
+    with app.app_context():
+        cv_to_pdf("es")
 
