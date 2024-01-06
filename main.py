b8481ffaa6105da49d642d9e56961e8b02d649ea
ale <aledeum.saf@gmail.com>
2024-01-06 13:05:04 -0300

    did some stuff

--- main.py
+++ main.py
@@ -23,11 +23,16 @@ def modify_query(**new_values):
 
     return '{}?{}'.format(request.path, werkzeug.urls.urlencode(args))
 
+@app.template_global()
+def change_language(new_lang: str):
+    url = request.full_path.split("/", 2)
+    url[1] = new_lang
+    return "/".join(url)
 
 def go_to_default(page, theme=DEFAULT_THEME):
     lang = get_language()
     if page != "":
-        return redirect(f"/{page}/{lang}/?theme={theme}")
+        return redirect(f"/{lang}/{page}/?theme={theme}")
     else:
         return redirect(f"/{lang}/?theme={theme}")
 
@@ -48,7 +53,7 @@ def root(lang_or_page):
     
     
 
-@app.route("/about/<lang>/")
+@app.route("/<lang>/about/")
 def about(lang):
     if lang not in SUPPORTED_LANGUAGES:
         go_to_default('about')
@@ -56,42 +61,51 @@ def about(lang):
 
 
     
-@app.route("/translation/<lang>/")
+@app.route("/<lang>/translation/")
 def linguistics(lang):
     if lang not in SUPPORTED_LANGUAGES:
         return go_to_default("translation")
     return render_page("translation.html", lang)
 
 
-@app.route("/tech/<lang>/")
+@app.route("/<lang>/tech/projects/<project_name>/")
+def project(lang, project_name):
+    if lang not in SUPPORTED_LANGUAGES:
+        return go_to_default(f"tech/projects/{project_name}")
+    return render_page(f"projects/{project_name}.html", lang)
+
+@app.route("/<lang>/tech/")
 def tech(lang):
     if lang not in SUPPORTED_LANGUAGES:
         return go_to_default("tech")
     return render_page("tech.html", lang)
 
-@app.route("/blog/<lang>/")
+
+
+@app.route("/<lang>/blog/")
 def blog(lang):
     if lang not in SUPPORTED_LANGUAGES:
         return go_to_default("blog")
     return render_page("blog.html", lang)
 
-@app.route("/cv/<lang>/")
+@app.route("/<lang>/cv/")
 def cv(lang):
     if lang not in SUPPORTED_LANGUAGES:
             return go_to_default("cv")
     
     return render_page("cv.html", lang, "cv_data.yaml")
 
-@app.route("/gallery/<lang>/")
+@app.route("/<lang>/gallery/")
 def gallery(lang):
     if lang not in SUPPORTED_LANGUAGES:
         go_to_default('gallery')
     return render_page("gallery.html", lang)
 
-@app.route("/test/<lang>/")
+@app.route("/<lang>/test/")
 def test(lang):
-    #return  get_cv()
-    pass
+    if lang not in SUPPORTED_LANGUAGES:
+            return go_to_default("test")
+    return render_page(r"projects/mini_me.html", lang)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=True, port=5000)
